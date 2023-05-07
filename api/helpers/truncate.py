5d7dcc06afa167fb4558512ae1aed1d4597c1339
valed-dm <dmvaled@gmail.com>
2023-05-07 19:38:22 +0300

    pylint: unnecessary "elif" after "return" removed

--- api/helpers/truncate.py
+++ api/helpers/truncate.py
@@ -1,7 +1,7 @@
 def prepare_list(data) -> str:
     if len(data) == 0:
         return ""
-    elif len(data) == 1:
+    if len(data) == 1:
         return data[0][:14]
-    else:
-        return data[0][:12] + f"..+{len(data) - 1}"
+
+    return data[0][:12] + f"..+{len(data) - 1}"
