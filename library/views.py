391ed15913b151afacfd57d3ae6c66a73306804c
valed-dm <dmvaled@gmail.com>
2023-05-08 11:40:47 +0300

    pylint: C0103: invalid-name Argument name "r" doesn't conform to snake_case naming style; r -> req

--- library/views.py
+++ library/views.py
@@ -28,12 +28,12 @@ class LibraryListView(ListView):
 
     def get_queryset(self):
         lib = library_to_view(self.request)
-        r = ArgsLibrary()
+        req = ArgsLibrary()
         if self.request.method == "POST":
-            r = get_args_library(self.request)
-        books_cards_sorted = sort_books(r, library_books=lib.books_cards)
+            req = get_args_library(self.request)
+        books_cards_sorted = sort_books(req, library_books=lib.books_cards)
         lib.books_cards = books_cards_sorted
-        info_sort_library(self.request, r)
+        info_sort_library(self.request, req)
         return lib
 
     def get_context_data(self, *args, **kwargs):
