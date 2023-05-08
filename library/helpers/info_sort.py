84e015fb67601e663519f49ee0686dc4826b44c2
valed-dm <dmvaled@gmail.com>
2023-05-08 09:06:12 +0300

    pylint: W1309: f-string-without-interpolation

--- library/helpers/info_sort.py
+++ library/helpers/info_sort.py
@@ -3,8 +3,8 @@ from django.contrib import messages
 
 def info_sort_library(request, inp):
     if inp.rubric is None:
-        messages.info(request, f"Library shown in fifo order:")
+        messages.info(request, "Library shown in fifo order:")
     else:
-        messages.info(request, f"Library filtered with params: ")
+        messages.info(request, "Library filtered with params: ")
         messages.info(request, f"rubric: {inp.rubric}, category: {inp.category}")
         messages.info(request, f"author: {inp.author}, sort: {inp.sort}")
