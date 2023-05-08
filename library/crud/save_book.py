43eed15d8e762add322b89d7177805b966e05629
valed-dm <dmvaled@gmail.com>
2023-05-08 13:19:41 +0300

    authors_str default value added

--- library/crud/save_book.py
+++ library/crud/save_book.py
@@ -1,13 +1,19 @@
 from library.models import Book
 
 
-def save_book_to_library(book, authors_str, image_src, authors, categories):
+def save_book_to_library(
+    book,
+    image_src,
+    authors,
+    categories,
+    authors_str="-no author-",
+):
     book_to_library = Book(
         authors_str=authors_str,
-        title=book["title"],
-        description=book["description"],
-        date=book["date"],
-        google_book_id=book["google_book_id"],
+        title=book.title,
+        description=book.description,
+        date=book.date,
+        google_book_id=book.google_book_id,
         rubric="read_asap",
         remark="no remark added",
         image_src=image_src,
