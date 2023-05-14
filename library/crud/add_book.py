d9d1e577ce882300b097413c872f5bfbcca6e471
valed-dm <dmvaled@gmail.com>
2023-05-14 07:01:22 +0300

    book as dict -> book as obj

--- library/crud/add_book.py
+++ library/crud/add_book.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect
 
+from api.schemas import Book
 from library.helpers.authors_str import authors_string
 from library.helpers.info_add import info_add
 from library.helpers.info_exists import info_exists
-from library.models import Author, Book, Category, Image
+from library.models import Author, Book as DB_Book, Category, Image
 from .get_or_create import get_or_create
 from .save_book import save_book_to_library
 
@@ -19,7 +20,7 @@ def add_book(request, book):
     authors_str = authors_string(book_authors)
     # checks if book already added to db before
     book_exists = (
-        Book.objects.all().filter(google_book_id=book["google_book_id"]).exists()
+        DB_Book.objects.all().filter(google_book_id=book["google_book_id"]).exists()
     )
     # fills in Image table to provide data for 1-2-1 image_src field
     image_src = Image(image_src=book["image_src"])
@@ -31,7 +32,13 @@ def add_book(request, book):
         info_exists(request, book["title"])
         return redirect("book/")
 
-    save_book_to_library(book, image_src, authors, categories, authors_str)
+    book_to_library = Book()
+    book_to_library.title = book["title"]
+    book_to_library.description = book["description"]
+    book_to_library.date = book["date"]
+    book_to_library.google_book_id = book["google_book_id"]
+
+    save_book_to_library(book_to_library, image_src, authors, categories, authors_str)
     info_add(request, book["title"])
 
     return book
